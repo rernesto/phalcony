e8ddb61ffa501665e0baf6bb1a6f52665e4adc6d
rernesto <rernesto.espinosa@gmail.com>
2022-01-14 23:56:14 -0500

    Added JQuery, Bootstrap and other initial front-end dependencies. Renamed `bootstrap.js` (stimulus bootstrap) to `stimulus.js`.

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,11 @@
-// start the Stimulus application
-import './bootstrap';
+import './styles/scss/app.scss';
+import './stimulus';
+
+let $ = require('jquery');
+global.$ = global.jQuery = $;
+window.$ = window.jQuery = $;
+
+require('bootstrap');
 
 const imagesContext = require.context('./images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
 imagesContext.keys().forEach(imagesContext);
